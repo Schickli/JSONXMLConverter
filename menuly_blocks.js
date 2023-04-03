c0a812cde216eccba4e405797a76f26dbe849c04
maurin schickli <maurin.schickli@gmail.com>
2023-04-03 17:12:47 +0200

    updates

--- menuly_blocks.js
+++ menuly_blocks.js
@@ -1,180 +1,270 @@
-'use strict';
+"use strict";
 
+Blockly.selectionArrow = function () {
+  return Blockly.RTL ? "←" : "→";
+};
+Blockly.keyValueArrow = function () {
+  return Blockly.RTL ? "⇐" : "⇒";
+};
 
-Blockly.selectionArrow  = function() { return Blockly.RTL ? "←" : "→"; };
-Blockly.keyValueArrow   = function() { return Blockly.RTL ? "⇐" : "⇒"; };
-
-
-Blockly.Blocks['start'] = {
-  init: function() {
+Blockly.Blocks["start"] = {
+  init: function () {
     this.setColour(250);
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("JSON structure");
+      .setAlign(Blockly.ALIGN_CENTRE)
+      .appendField("JSON structure");
 
-    this.appendValueInput('json')
-        .appendSelector(['dictionary', 'array', 'string', 'number', 'true', 'false'], Blockly.selectionArrow(), 'null');
+    this.appendValueInput("json").appendSelector(
+      ["dictionary", "array", "string", "number", "true", "false", "node"],
+      Blockly.selectionArrow(),
+      "null"
+    );
 
     this.setDeletable(false);
-  }
+  },
 };
 
-
-Blockly.Blocks['true'] = {
-  init: function() {
+Blockly.Blocks["true"] = {
+  init: function () {
     this.setColour(20);
     this.setOutput(true, ["element"]);
 
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField('true');
-  }
+    this.appendDummyInput().setAlign(Blockly.ALIGN_CENTRE).appendField("true");
+  },
 };
 
-
-Blockly.Blocks['false'] = {
-  init: function() {
+Blockly.Blocks["false"] = {
+  init: function () {
     this.setColour(20);
     this.setOutput(true, ["element"]);
 
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField('false');
-  }
+    this.appendDummyInput().setAlign(Blockly.ALIGN_CENTRE).appendField("false");
+  },
 };
 
-
-Blockly.Blocks['string'] = {
-  init: function() {
+Blockly.Blocks["string"] = {
+  init: function () {
     this.setColour(190);
     this.setOutput(true, ["element"]);
 
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField('"')
-        .appendField(new Blockly.FieldTextInput(''), 'string_value')
-        .appendField('"');
-  }
+      .setAlign(Blockly.ALIGN_CENTRE)
+      .appendField('"')
+      .appendField(new Blockly.FieldTextInput(""), "string_value")
+      .appendField('"');
+  },
 };
 
-
-Blockly.Blocks['number'] = {
-  init: function() {
+Blockly.Blocks["number"] = {
+  init: function () {
     this.setColour(210);
     this.setOutput(true, ["element"]);
 
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField('№')
-        .appendField(new Blockly.FieldTextInput('0', Blockly.FieldTextInput.numberValidator), "number_value");
-  }
+      .setAlign(Blockly.ALIGN_CENTRE)
+      .appendField("№")
+      .appendField(
+        new Blockly.FieldTextInput("0", Blockly.FieldTextInput.numberValidator),
+        "number_value"
+      );
+  },
 };
 
-
-Blockly.Blocks['dictionary'] = {
+Blockly.Blocks["dictionary"] = {
   length: 0,
-  init: function() {
+  init: function () {
     this.setColour(120);
     this.setOutput(true, ["element"]);
 
-    this.appendDummyInput('open_bracket')
-        .appendField(" { ")
-        .appendField(new Blockly.FieldTextbutton('+', function() { this.sourceBlock_.appendKeyValuePairInput(); }) );
+    this.appendDummyInput("open_bracket")
+      .appendField(" { ")
+      .appendField(
+        new Blockly.FieldTextbutton("+", function () {
+          this.sourceBlock_.appendKeyValuePairInput();
+        })
+      );
 
-    this.appendDummyInput('close_bracket')
-        .appendField(" } ");
+    this.appendDummyInput("close_bracket").appendField(" } ");
 
     this.setInputsInline(false);
   },
 
-  appendKeyValuePairInput: function() {
+  appendKeyValuePairInput: function () {
+    var lastIndex = this.length++;
 
-        var lastIndex = this.length++;
+    var appended_input = this.appendValueInput("element_" + lastIndex);
+    appended_input
+      .appendField(
+        new Blockly.FieldTextbutton("–", function () {
+          this.sourceBlock_.deleteKeyValuePairInput(appended_input);
+        })
+      )
+      .appendField(
+        new Blockly.FieldTextInput("key_" + lastIndex),
+        "key_field_" + lastIndex
+      )
+      .appendField(Blockly.keyValueArrow())
+      .appendSelector(
+        ["string", "number", "true", "false", "dictionary", "array"],
+        Blockly.selectionArrow(),
+        "null"
+      );
 
-        var appended_input = this.appendValueInput('element_'+lastIndex);
-        appended_input.appendField(new Blockly.FieldTextbutton('–', function() { this.sourceBlock_.deleteKeyValuePairInput(appended_input); }) )
-            .appendField(new Blockly.FieldTextInput('key_'+lastIndex), 'key_field_'+lastIndex)
-            .appendField( Blockly.keyValueArrow() )
-            .appendSelector(['string', 'number', 'true', 'false', 'dictionary', 'array'], Blockly.selectionArrow(), 'null');
+    this.moveInputBefore("element_" + lastIndex, "close_bracket");
 
-        this.moveInputBefore('element_'+lastIndex, 'close_bracket');
-
-        return appended_input;
+    return appended_input;
   },
 
-  deleteKeyValuePairInput: function(inputToDelete) {
+  deleteKeyValuePairInput: function (inputToDelete) {
+    var inputNameToDelete = inputToDelete.name;
 
-        var inputNameToDelete = inputToDelete.name;
+    var substructure = this.getInputTargetBlock(inputNameToDelete);
+    if (substructure) {
+      substructure.dispose(true, true);
+    }
+    this.removeInput(inputNameToDelete);
 
-        var substructure = this.getInputTargetBlock(inputNameToDelete);
-        if(substructure) {
-            substructure.dispose(true, true);
-        }
-        this.removeInput(inputNameToDelete);
+    var inputIndexToDelete = parseInt(inputToDelete.name.match(/\d+/)[0]);
 
-        var inputIndexToDelete = parseInt(inputToDelete.name.match(/\d+/)[0]);
+    var lastIndex = --this.length;
 
-        var lastIndex = --this.length;
+    for (var i = inputIndexToDelete + 1; i <= lastIndex; i++) {
+      // rename all the subsequent element-inputs
+      var input = this.getInput("element_" + i);
+      input.name = "element_" + (i - 1);
 
-        for(var i=inputIndexToDelete+1; i<=lastIndex; i++) { // rename all the subsequent element-inputs
-            var input       = this.getInput( 'element_'+i );
-            input.name      = 'element_'+(i-1);
-
-            var key_field   = this.getField_( 'key_field_'+i );
-            key_field.name  = 'key_field_'+(i-1);
-        }
-  }
+      var key_field = this.getField_("key_field_" + i);
+      key_field.name = "key_field_" + (i - 1);
+    }
+  },
 };
 
-
-Blockly.Blocks['array'] = {
+Blockly.Blocks["node"] = {
   length: 0,
-  init: function() {
+  init: function () {
+    this.setColour(50);
+    this.setOutput(true, ["element"]);
+
+    this.appendDummyInput("open_bracket")
+      .appendField(" Node ")
+      .appendField(
+        new Blockly.FieldTextbutton("+", function () {
+          this.sourceBlock_.appendKeyValuePairInput();
+        })
+      );
+
+    
+    this.setInputsInline(false);
+  },
+
+  appendKeyValuePairInput: function (params) {
+    var lastIndex = this.length++;
+
+    var appended_input = this.appendValueInput("element_" + lastIndex);
+    appended_input
+      .appendField(
+        new Blockly.FieldTextbutton("–", function () {
+          this.sourceBlock_.deleteKeyValuePairInput(appended_input);
+        })
+      )
+      .appendField(
+        new Blockly.FieldTextInput("@" + params),
+        "key_field_" + lastIndex
+      )
+      .appendField(Blockly.keyValueArrow())
+      .appendSelector(
+        ["string", "number", "true", "false", "dictionary", "array", "node"],
+        Blockly.selectionArrow(),
+        "null"
+      );
+
+    this.moveInputBefore("element_" + lastIndex, "close_bracket");
+
+    return appended_input;
+  },
+
+  deleteKeyValuePairInput: function (inputToDelete) {
+    var inputNameToDelete = inputToDelete.name;
+
+    var substructure = this.getInputTargetBlock(inputNameToDelete);
+    if (substructure) {
+      substructure.dispose(true, true);
+    }
+    this.removeInput(inputNameToDelete);
+
+    var inputIndexToDelete = parseInt(inputToDelete.name.match(/\d+/)[0]);
+
+    var lastIndex = --this.length;
+
+    for (var i = inputIndexToDelete + 1; i <= lastIndex; i++) {
+      // rename all the subsequent element-inputs
+      var input = this.getInput("element_" + i);
+      input.name = "element_" + (i - 1);
+
+      var key_field = this.getField_("key_field_" + i);
+      key_field.name = "key_field_" + (i - 1);
+    }
+  },
+};
+
+Blockly.Blocks["array"] = {
+  length: 0,
+  init: function () {
     this.setColour(350);
     this.setOutput(true, ["element"]);
 
-    this.appendDummyInput('open_bracket')
-        .appendField(" [ ")
-        .appendField(new Blockly.FieldTextbutton('+', function() { this.sourceBlock_.appendArrayElementInput(); }) );
+    this.appendDummyInput("open_bracket")
+      .appendField(" [ ")
+      .appendField(
+        new Blockly.FieldTextbutton("+", function () {
+          this.sourceBlock_.appendArrayElementInput();
+        })
+      );
 
-    this.appendDummyInput('close_bracket')
-        .appendField(" ] ");
+    this.appendDummyInput("close_bracket").appendField(" ] ");
 
     this.setInputsInline(false);
   },
 
-  appendArrayElementInput: function() {
+  appendArrayElementInput: function () {
+    var lastIndex = this.length++;
 
-        var lastIndex = this.length++;
+    var appended_input = this.appendValueInput("element_" + lastIndex);
+    appended_input
+      .appendField(
+        new Blockly.FieldTextbutton("–", function () {
+          this.sourceBlock_.deleteArrayElementInput(appended_input);
+        })
+      )
+      .appendSelector(
+        ["string", "number", "true", "false", "dictionary", "array"],
+        Blockly.selectionArrow(),
+        "null"
+      );
 
-        var appended_input = this.appendValueInput('element_'+lastIndex);
-        appended_input.appendField(new Blockly.FieldTextbutton('–', function() { this.sourceBlock_.deleteArrayElementInput(appended_input); }) )
-            .appendSelector(['string', 'number', 'true', 'false', 'dictionary', 'array'], Blockly.selectionArrow(), 'null');
+    this.moveInputBefore("element_" + lastIndex, "close_bracket");
 
-        this.moveInputBefore('element_'+lastIndex, 'close_bracket');
-
-        return appended_input;
+    return appended_input;
   },
 
-  deleteArrayElementInput: function(inputToDelete) {
+  deleteArrayElementInput: function (inputToDelete) {
+    var inputNameToDelete = inputToDelete.name;
 
-        var inputNameToDelete = inputToDelete.name;
+    var substructure = this.getInputTargetBlock(inputNameToDelete);
+    if (substructure) {
+      substructure.dispose(true, true);
+    }
+    this.removeInput(inputNameToDelete);
 
-        var substructure = this.getInputTargetBlock(inputNameToDelete);
-        if(substructure) {
-            substructure.dispose(true, true);
-        }
-        this.removeInput(inputNameToDelete);
+    var inputIndexToDelete = parseInt(inputToDelete.name.match(/\d+/)[0]);
 
-        var inputIndexToDelete = parseInt(inputToDelete.name.match(/\d+/)[0]);
+    var lastIndex = --this.length;
 
-        var lastIndex = --this.length;
+    for (var i = inputIndexToDelete + 1; i <= lastIndex; i++) {
+      // rename all the subsequent element-inputs
+      var input = this.getInput("element_" + i);
 
-        for(var i=inputIndexToDelete+1; i<=lastIndex; i++) { // rename all the subsequent element-inputs
-            var input  = this.getInput( 'element_'+i );
-
-            input.name = 'element_'+(i-1);
-        }
-  }
+      input.name = "element_" + (i - 1);
+    }
+  },
 };
-
